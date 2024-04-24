8f66aea9a9a7589c9abb63e3ab5648dfdcd940fb
orangemamal <ojm1612@gmail.com>
2024-04-24 20:50:44 +0900

    require to a dynamic import error fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,5 +33,7 @@ export default {
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: [({ isLegacy }) => isLegacy && 'axios']
+  },
 }
